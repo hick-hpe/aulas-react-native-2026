46dd1eaa444f22e4916333341b0c25ef9cc0cd32
hick-hpe <hpalermoemerick@gmail.com>
2026-04-16 15:53:32 -0300

    feat: expo router example

---
+++ meuApp/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+
+export default function Layout() {
+    return (
+        <Stack
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Stack.Screen name="dashboard" options={{ headerShown: false }} />
+        </Stack>
+    )
+}
